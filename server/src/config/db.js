5d74e97e1dd2b06d92d23f653031e1c8558af281
ShriRam <shri2703ram@gmail.com>
2026-01-22 15:22:10 +0530

    COMMIT

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error(err));
+;
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
